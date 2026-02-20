82f7a57b1469de011763dad2c683228363ea6cec
Sizzad Hosen <sizzadhosenofficial@gmail.com>
2026-02-20 20:48:56 +0600

    create the model and controller in room , roomalloance, payment rent

---
+++ app/Http/Controllers/PaymentsRentController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\PaymentsRent;
+use Illuminate\Http\Request;
+
+class PaymentsRentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(PaymentsRent $paymentsRent)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(PaymentsRent $paymentsRent)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, PaymentsRent $paymentsRent)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(PaymentsRent $paymentsRent)
+    {
+        //
+    }
+}
